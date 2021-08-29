95d839d39c827a5d6127191a87f7fae46365dade
Stella Oliveira <stella.1609@gmail.com>
2021-08-29 16:30:41 -0300

    update composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f10e7d51e6df6744e556cbcf70c580b3e07bc1ad',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f10e7d51e6df6744e556cbcf70c580b3e07bc1ad',
+            'dev_requirement' => false,
+        ),
+    ),
+);
