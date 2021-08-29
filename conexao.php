5d5b3aafffe1f00dfd148983ff3e0d30e35b04c7
Stella Oliveira <stella.1609@gmail.com>
2021-08-29 16:16:33 -0300

    update database

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 
-    $servidor = "localhost";
-    $usuario = "root";
-    $senha = "";
-    $database = "desafiogama";
+    $servidor = env('DB_HOST', 'remotemysql.com');
+    $usuario = env('DB_USERNAME', 'VeBMhNuNpU'),
+    $senha = env('DB_PASSWORD', 'A4azChwX0M'),
+    $database = env('DB_DATABASE', 'VeBMhNuNpU');
     $conexao = mysqli_connect($servidor, $usuario, $senha, $database);
 
     if(!$conexao){
