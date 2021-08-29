0b7599ce2ca478aaf957a56114779ad5632f108d
Stella Oliveira <stella.1609@gmail.com>
2021-08-29 20:27:21 -0300

    Update processa.php

--- processa.php
+++ processa.php
@@ -52,18 +52,6 @@
 	    <link rel="stylesheet" type="text/css" href="./Estilo/formulario.css" /> 
         <link rel="stylesheet" type="text/css" href="./Estilo/estilo.css" />
 
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-
-        <script src="./JS/jQuery Validation/lib/jquery.js"></script>
-   		<script src="https://jqueryvalidation.org/files/lib/jquery-1.11.1.js"></script>
-        <script src="./JS/jQuery Validation/dist/jquery.validate.js"></script>
-
-        <script type="text/javascript" src="./JS/validarForm.js"></script>
-        <script type="text/javascript" src="./JS/cep.js"></script>
-        <script type="text/javascript" src="./JS/casaouap.js"></script>
-        <script type="text/javascript" src="./JS/veiculo.js"></script>
-
         <script src="https://kit.fontawesome.com/ef7a547aae.js" crossorigin="anonymous"></script>
 	</head>
 
